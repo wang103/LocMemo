1cbdb8eeaa4d02ccdf350a207d411534c2c17ca2
Tianyi Wang <wang103uiuc@gmail.com>
2020-04-03 13:42:35 -0700

    First commit

---
+++ LocMemo/LocMemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocMemo
+//
+//  Created by Tianyi Wang on 4/3/20.
+//  Copyright © 2020 x. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+https://docs.google.com/document/d/1Mg54RBZgEJbRB8kChU5Nj682Y3mC5mdAScPQKXLaJSg/edit?usp=sharing

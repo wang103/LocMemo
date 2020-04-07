4ab751ecad65a3a45dc5d34ff354409d757d45c2
Tianyi Wang <wang103uiuc@gmail.com>
2020-04-06 18:43:37 -0700

    Intro DataManager

---
+++ LocMemo/LocMemo/Data/DataManager.swift
@@ -0,0 +1,22 @@
+//
+//  DataManager.swift
+//  LocMemo
+//
+//  Created by Tianyi Wang on 4/6/20.
+//  Copyright © 2020 x. All rights reserved.
+//
+
+import Foundation
+
+class DataManager {
+    static let shared = DataManager()
+
+    func saveLocMemo(identifier: String,
+                     locationText: String,
+                     memoText: String,
+                     status: LocMemoStatus,
+                     createdAt: Date,
+                     updatedAt: Date) {
+
+    }
+}
---
+++ LocMemo/LocMemo/Data/LocMemoStatus.swift
@@ -0,0 +1,12 @@
+//
+//  LocMemoStatus.swift
+//  LocMemo
+//
+//  Created by Tianyi Wang on 4/6/20.
+//  Copyright © 2020 x. All rights reserved.
+//
+
+enum LocMemoStatus: Int16 {
+    case active   = 0
+    case inactive = 1
+}

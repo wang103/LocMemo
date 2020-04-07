bd47576623d924afcf673ac8f8cec05f97b0d1c8
Tianyi Wang <wang103uiuc@gmail.com>
2020-04-06 21:08:16 -0700

    MemosView UI in progress

--- LocMemo/LocMemo/Views/MemosView.swift
+++ LocMemo/LocMemo/Views/MemosView.swift
@@ -9,22 +9,41 @@
 import SwiftUI
 
 struct MemosView: View {
-    var body: some View {
-        NavigationView {
-            Form {
-                Section {
-                    Text("Hello")
-                    Text("Hello")
-                    Text("Hello")
-                }
 
-                Section {
-                    Text("Hello")
-                    Text("Hello")
-                    Text("Hello")
+    @State private var showError: Bool = false
+    @State private var errMsg: String = ""
+
+    var body: some View {
+        let locMemos = getAllLocMemos()
+
+        return NavigationView {
+            List {
+                ForEach(0..<locMemos.count) { index in
+                    Text(locMemos[index].locationText)
                 }
             }
             .navigationBarTitle("Memos")
         }
+        .alert(isPresented: self.$showError, content: self.getErrorAlert)
+    }
+
+    func getAllLocMemos() -> [LocMemoData] {
+        do {
+            return try DataManager.shared.getAllLocMemos()
+        } catch let error as NSError {
+            showErrorMsg("Unable to read saved memos. Please try again. \(error.localizedDescription)")
+            return []
+        }
+    }
+
+    func showErrorMsg(_ msg: String) {
+        errMsg = msg
+        showError = true
+    }
+
+    func getErrorAlert() -> Alert {
+        return Alert(title: Text("Error!"),
+                     message: Text(errMsg),
+                     dismissButton: .default(Text("OK")))
     }
 }

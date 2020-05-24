1cdf30073ac9454ec7f6e5cef0e0fe68abb19735
Tianyi Wang <wang103uiuc@gmail.com>
2020-05-24 01:43:12 -0700

    Revert "Baidu map progress"
    This reverts commit 2632791d251b39de2ed40a6f62328e7053b65339.

--- LocMemo/LocMemo/Location/AppleLocationSearcher.swift
+++ LocMemo/LocMemo/Location/AppleLocationSearcher.swift
@@ -13,37 +13,13 @@ class AppleLocationSearcher: LocationSearcher {
                        completionHandler: @escaping ([CLPlacemark]?, NSError?) -> Void) {
         let locale = Locale.current
         let geocoder = CLGeocoder()
-        geocoder.geocodeAddressString(addressString, in: nil, preferredLocale: locale) { (_placemarks, error) in
+        geocoder.geocodeAddressString(addressString, in: nil, preferredLocale: locale) { (placemarks, error) in
             if error == nil {
-
-                var placemarks = _placemarks
-                if placemarks != nil {
-                    // All placemark coordinates fetched from Apple is in WGS84. However, in China,
-                    // Apple Map would use GCJ02. So convert if needed now.
-                    placemarks = placemarks!.map({self.toChinaPlacemark($0)})
-                }
-
                 completionHandler(placemarks, nil)
                 return
             }
 
             completionHandler(nil, error as NSError?)
         }
-    }
-
-    fileprivate func toChinaPlacemark(_ placemark: CLPlacemark) -> CLPlacemark {
-        if placemark.isoCountryCode == nil || placemark.isoCountryCode! != "CN" {
-            return placemark
-        }
-
-        var oldCenter = (placemark.region as! CLCircularRegion).center
-        oldCenter = CLLocationCoordinate2DMake(oldCenter.latitude, oldCenter.longitude)
-        let newCenter = toGcj02(oldCenter)
-        let newLocation = CLLocation(latitude: newCenter.latitude, longitude: newCenter.longitude)
-        return CLPlacemark(location: newLocation, name: placemark.name, postalAddress: placemark.postalAddress)
-    }
-
-    fileprivate func toGcj02(_ wgs84: CLLocationCoordinate2D) -> CLLocationCoordinate2D {
-        return BMKCoordTrans(wgs84, .COORDTYPE_GPS, .COORDTYPE_COMMON)
     }
 }
